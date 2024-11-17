e0800c366f977810c35a91bdd9f5896bd853685c
Danila <silnovdanila2@gmail.com>
2024-11-17 11:13:36 +0300

    Добавлены структуры

--- lab2/Kafe.cpp
+++ lab2/Kafe.cpp
@@ -1,4 +1,39 @@
 #include "Kafe.h"
 #include <stdio.h>
 
-
+struct Dish {
+	int price;
+	char name[20];
+};
+struct Employee {
+	int salary;
+	char job[20];
+	char name[20];
+};
+struct Order {
+	struct Dish Dishes[20];
+};
+struct Chef {
+	struct Employee chef;
+	struct Dish currentDish;
+};
+struct Menu{
+	struct Dish hotDishes[10];
+	struct Dish desserts[10];
+	struct Dish drinks[10];
+};
+struct DiscountCard {
+	int id;
+	char name[20];
+	double discount;
+	struct Order historyOForders[20];
+};
+struct Client {
+	struct Order currentOrder;
+	struct DiscountCard Card;
+};
+struct Waiter {
+	struct Employee waiter;
+	struct Client currentClient;
+	struct Order currentOrder;
+};
--- lab2/Kafe.h
+++ lab2/Kafe.h
@@ -1,4 +1,11 @@
 #pragma once
 
 struct Dish;
+struct Employee;
+struct Order;
+struct Chef;
+struct Menu;
+struct DiscountCard;
+struct Waiter;
+struct Client;
 
